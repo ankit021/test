515d0afbeb0a93ac410075ecbf5aa8735245f21a
ankit021 <50499202+ankit021@users.noreply.github.com>
2019-10-07 13:11:48 +0530

    Add files via upload

---
+++ SpringMVC-App1/src/main/java/com/eclature/spring/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.eclature.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	
+	@RequestMapping("/welcome")
+	public String greet(Model model){
+		model.addAttribute("msg", "Welcome to Spring MVC..!!");
+		return "Welcome";
+	}
+
+}
